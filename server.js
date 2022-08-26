24c68cae2053835143873c5487c99ae17ce9a8f6
rafael bastos de sousa <rafael.bastos@gestaokaizen.com.br>
2022-08-25 21:40:04 -0300

    first awesome commit

---
+++ server.js
@@ -0,0 +1,49 @@
+require('dotenv').config()
+var express = require('express');
+var bodyParser = require('body-parser')
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mongoose = require('mongoose');
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+
+var dbUrl = process.env.URLMONGODB;
+
+mongoose.connect(dbUrl, {}).then(() => console.log("Mongoose connected"));
+
+
+var Message = mongoose.model('Message',{
+  name : String,
+  message : String
+})
+
+app.get('/messages', (req, res) => {
+  Message.find({},(err, messages)=> {
+    res.send(messages);
+  })
+})
+app.get('/messages', (req, res) => {
+  Message.find({},(err, messages)=> {
+    res.send(messages);
+  })
+})
+app.post('/messages', (req, res) => {
+  var message = new Message(req.body);
+  message.save((err) =>{
+    if(err)
+      sendStatus(500);
+    io.emit('message', req.body);
+    res.sendStatus(200);
+  })
+})
+io.on('connection', () =>{
+  console.log('a user is connected')
+})
+// mongoose.connect(dbUrl ,{useMongoClient : true} ,(err) => {
+//   console.log('mongodb connected',err);
+// })
+var server = http.listen(3000, () => {
+  console.log('server is running on port', server.address().port);
+});
